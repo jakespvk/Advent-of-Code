551cd2acc6d578ceb0cb4b82b94db63d3bc71e2c
Jake Spievak <jakespvk@gmail.com>
2024-03-08 20:32:29 -0800

    new stuff

--- day2/pt1.py
+++ day2/pt1.py
@@ -2,4 +2,7 @@ f = open("input.txt", "r")
 input = f.readlines()
 f.close()
 
+available_cubes = { "red" : 12, "green" : 13, "blue" : 14 }
 
+for line in input:
+    # find games where there are less than the available cubes
