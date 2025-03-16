0341b97255555d8ee9e35cbb3840b9e726053c22
Jake Spievak <jakespvk@gmail.com>
2025-03-15 18:14:59 -0700

    day1 part1&2 done

--- day1/part1/main.zig
+++ day1/part1/main.zig
@@ -1,5 +1,42 @@
 const std = @import("std");
+////
+const INPUT_FILE = "input.txt";
+////
+const COUNT_FILE_LINES = if (std.mem.eql(u8, INPUT_FILE, "input.txt")) 1000 else 6;
+const INT_SIZE = if (std.mem.eql(u8, INPUT_FILE, "input.txt")) i18 else i5;
 
 pub fn main() !void {
-    std.debug.print("hello world\n", .{});
+    const file = std.fs.cwd().openFile(INPUT_FILE, .{}) catch |err| {
+        std.log.err("Failed to open file: {s}", .{@errorName(err)});
+        return;
+    };
+    defer file.close();
+
+    std.debug.print("{d}\n", .{std.math.maxInt(INT_SIZE)});
+
+    var left_nums: [COUNT_FILE_LINES]INT_SIZE = undefined;
+    var right_nums: [COUNT_FILE_LINES]INT_SIZE = undefined;
+    var buffer: [128]u8 = undefined;
+    var fba = std.heap.FixedBufferAllocator.init(&buffer);
+    const allocator = fba.allocator();
+    var idx: usize = 0;
+    while (file.reader().readUntilDelimiterOrEofAlloc(allocator, '\n', std.math.maxInt(usize)) catch |err| {
+        std.log.err("Failed to read line: {s}", .{@errorName(err)});
+        return;
+    }) |line| {
+        defer allocator.free(line);
+        left_nums[idx] = try std.fmt.parseInt(INT_SIZE, line[0 .. std.mem.indexOf(u8, line, " ") orelse unreachable], 0);
+        right_nums[idx] = try std.fmt.parseInt(INT_SIZE, line[1 + (std.mem.lastIndexOf(u8, line, " ") orelse unreachable) ..], 0);
+        std.debug.print("{d} {d}\n", .{ left_nums[idx], right_nums[idx] });
+        idx += 1;
+    }
+    std.mem.sort(INT_SIZE, &left_nums, {}, std.sort.asc(INT_SIZE));
+    std.mem.sort(INT_SIZE, &right_nums, {}, std.sort.asc(INT_SIZE));
+
+    var sum: u64 = 0;
+    for (left_nums, 0..) |_, i| {
+        sum += @abs(left_nums[i] - right_nums[i]);
+        std.debug.print("left: {d}\nright: {d}\nrunning sum: {d}\n", .{ left_nums[i], right_nums[i], @abs(left_nums[i] - right_nums[i]) });
+    }
+    std.debug.print("\nfinal sum: {d}\n", .{sum});
 }
